6ab49d1390f84cbb6264eb6f1feb437f5376174c
Vojtěch Kulvait <kulvait@gmail.com>
2025-10-06 15:19:26 +0200

    Site development

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import AppMainTab from './AppMainTab';
 
 import './global.css'; // Ensure Tailwind and DaisyUI styles are imported
 
@@ -8,10 +9,10 @@ const App: React.FC = () => {
   return (
     <div className="min-h-screen bg-base-200 text-base-content">
       {/* Navbar */}
-      <div className="navbar bg-primary px-6 shadow-lg">
+      <div className="navbar bg-primary px-6 shadow-lg w-full">
         <div className="flex-1">
           <a
-            className="text-white font-extrabold text-4xl drop-shadow-lg select-none"
+            className="text-white font-bold text-4xl drop-shadow-lg select-none"
             href="#"
           >
             AMBCAT Explorer
@@ -19,16 +20,28 @@ const App: React.FC = () => {
         </div>
         <div className="flex-none gap-2">
           <button
-            className="btn btn-sm btn-accent text-xl"
+            className="btn btn-sm text-xl"
             onClick={() => setActiveTab("about")}
           >
             About
           </button>
+          
+         <a
+            href="https://github.com/hereon-GEMS/ambcat-explorer"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-sm text-xl"
+          >
+            GitHub
+          </a>
+
+
+
           <a
             href="./ambivator/"
             target="_blank"
             rel="noopener noreferrer"
-            className="btn btn-sm btn-outline btn-ghost text-xl"
+            className="btn btn-sm btn-primary text-xl"
           >
             Ambivator
           </a>
@@ -39,12 +52,12 @@ const App: React.FC = () => {
       <main className="container mx-auto p-6">
         {activeTab === "about" && (
           <>
-            <h1 className="text-3xl font-bold mb-4">🧬 AMBCAT Explorer</h1>
             <p className="mb-6 text-lg leading-relaxed">
               <strong>AMBCAT</strong> is an Open Science initiative to digitize and
               explore <strong>3D scans of amber fossils</strong>, providing a window
               into ancient ecosystems through exceptional fossil preservation.
             </p>
+			<AppMainTab />
             {/* Add more about content here */}
           </>
         )}
